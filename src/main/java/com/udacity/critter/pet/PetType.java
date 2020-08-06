29cbcfc23d93a3fe965df98d3b0d7e5f160c33a2
Nitish Dhiman <nitishdhiman321@gmail.com>
2020-08-06 13:24:12 +0530

    passed project

---
+++ src/main/java/com/udacity/critter/pet/PetType.java
@@ -0,0 +1,9 @@
+package com.udacity.critter.pet;
+
+//A example list of pet type metadata that could be included on a request to create a pet.
+ 
+public enum PetType {
+
+     DOG, CAT, LIZARD, BIRD, FISH, SNAKE, OTHER;
+
+}
